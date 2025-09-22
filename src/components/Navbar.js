0c4f11eb3dfdf58b484621b2e45c48b00a5fc8a1
Mr-Edd834 <kagorieddy76@gmail.com>
2025-09-23 01:36:55 +0300

    updated names

---
+++ src/components/Navbar.js
@@ -0,0 +1,11 @@
+import React from "react";  
+  
+const navbar = () => {  
+  return (  
+    <nav className="navbar">
+        <h1>Home</h1> 
+    </nav>
+  );  
+}   
+
+export default navbar;  
---
+++ src/components/home.js
@@ -0,0 +1,12 @@
+import react from 'react';
+
+const home=()=>{
+  return (
+    <div className="home-container">
+      <h1>Welcome to the Home Page</h1>
+      <p>This is the home page of our application.</p>
+    </div>
+  );
+}
+
+export default home;    
